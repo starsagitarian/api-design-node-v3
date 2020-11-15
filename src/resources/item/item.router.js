9b686f16024f213010f8df65cf2ad3bfb8d67767
Baiju Joseph <baiju.joseph@gmail.com>
2020-11-15 20:26:08 +0000

    new

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,35 @@
-import { Router } from 'express'
+import { Router } from 'express';
+import { get } from 'lodash';
 
-const router = Router()
+const router = Router();
 
-export default router
+const controller = (req, res) => {
+    res.send({ 'message': 'jello!!'}); 
+}
+
+router.route('/api')
+      .get(controller) // this is get may
+      .post(controller); // for CREAT
+
+router.route('/api/:id')
+      .put(controller) // for update
+      .delete(controller) //for delete
+      get(controller); // read -> for get one
+
+
+// routes not needing individual id
+router.route('/')
+      .get() // Read -> for many
+      .post(); // Create 
+
+
+// routes were individual id is needed
+router.route('/:id')
+      .get() // Read -> for one
+      .put() // Update
+      .delete() // delete
+
+//C R U D 
+
+
+export default router;
--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
-import express from 'express'
+import express, { Router } from 'express'
 import { json, urlencoded } from 'body-parser'
 import morgan from 'morgan'
 import config from './config'
 import cors from 'cors'
 import { connect } from './utils/db'
+import itemRouter from './resources/item/item.router';
 
 export const app = express()
 
@@ -13,14 +14,15 @@ app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
+app.use(itemRouter);
 
 export const start = async () => {
   try {
     await connect()
     app.listen(config.port, () => {
-      console.log(`REST API on http://localhost:${config.port}/api`)
+      console.log(`REST API on http://localhost:${config.port}/api`);
     })
-  } catch (e) {
-    console.error(e)
+  } catch (err) {
+    console.error(err);
   }
 }
